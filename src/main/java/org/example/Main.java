d2cae1a4d2337691e8c70b60b880910fb172fef0
Marcus <marcus.henriksson@restory.ai>
2024-01-31 10:29:44 +0100

    Edited .yml file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        //Print out to Console
         System.out.println("Hello world!");
+
+        //Create POJO and call function
+        Messenger msg = new Messenger();
+        System.out.println(msg.sendMessage());
     }
 }
